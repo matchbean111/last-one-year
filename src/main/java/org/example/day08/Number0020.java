cb15a58d613a5a258541312e028ad043cddd69d3
matchbean111 <1224231492@qq.com>
2024-01-13 21:30:16 +0800

    ooo

---
+++ src/main/java/org/example/day08/Number0020.java
@@ -0,0 +1,40 @@
+package org.example.day08;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Number0020 {
+    public static void main(String[] args) {
+                String s = "{()[]]{}";
+        System.out.println(isValid(s));
+    }
+
+    private static boolean isValid(String s) {
+        int n = s.length();
+//        if (n % 2 == 1) return false;
+
+        LinkedList<Character> stack = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (stack.isEmpty()){
+                stack.push(ch);
+            }else {
+                char p = stack.getFirst();
+                if (p == '(' && ch - p == 1) {
+                    stack.pop();
+                } else if (p == '[' && ch - p == 2) {
+                    stack.pop();
+                } else if (p == '{' && ch - p == 2) {
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+                }
+            }
+        }
+        for (Character c : stack) {
+            System.out.print(c);
+        }
+        return stack.isEmpty();
+    }
+}
---
+++ src/main/java/org/example/day08/Number0150.java
@@ -0,0 +1,32 @@
+package org.example.day08;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class Number0150 {
+    public static void main(String[] args) {
+        String[] tokens = {"2", "1", "+", "3", "*"};
+        System.out.println(evalRPN(tokens));
+    }
+
+    private static int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new LinkedList();
+        for (String s : tokens) {
+            if ("+".equals(s)) {        // leetcode 内置jdk的问题，不能使用==判断字符串是否相等
+                stack.push(stack.pop() + stack.pop());      // 注意 - 和/ 需要特殊处理
+            } else if ("-".equals(s)) {
+                stack.push(-stack.pop() + stack.pop());
+            } else if ("*".equals(s)) {
+                stack.push(stack.pop() * stack.pop());
+            } else if ("/".equals(s)) {
+                int temp1 = stack.pop();
+                int temp2 = stack.pop();
+                stack.push(temp2 / temp1);
+            } else {
+                stack.push(Integer.valueOf(s));
+            }
+        }
+        return stack.pop();
+    }
+}
