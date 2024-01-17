73877afe30431f5d138b4129db132e8bc57ec98a
matchbean111 <1224231492@qq.com>
2024-01-18 03:59:43 +0800

    some fix

---
+++ src/test/java/org/example/day01/Number0035Test.java
@@ -0,0 +1,10 @@
+package org.example.day01;
+
+import org.junit.jupiter.api.Test;
+
+public class Number0035Test {
+    @Test
+    void testSearchInsert() {
+        System.out.println(123);
+    }
+}
